a5668c7096e476f0a3c0c2dadddca366825dd9b1
Rishu Tiwari <rishutiwari15062003@gmail.com>
2022-12-05 11:33:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def isPerfectSquare(x):
 	return s*s == x
 
 # Returns true if n is a Fibonacci Number, else false
-def isFibonacci(n):
+def isFi(n):
 
 	# n is Fibonacci if one of 5*n*n + 4 or 5*n*n - 4 or both
 	# is a perfect square
@@ -15,7 +15,7 @@ def isFibonacci(n):
 	
 # A utility function to test above functions
 for i in range(1,11):
-	if (isFibonacci(i) == True):
+	if (isFi(i) == True):
 		print i,"is a Fibonacci Number"
 	else:
 		print i,"is a not Fibonacci Number "
